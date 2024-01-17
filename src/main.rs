8212c47f708b13897c6a8d9ca7a632f1eef0b437
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-17 19:56:52 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,9 @@
     //const PI_NUMBER: f32 = 3.14159; // pi sayısı
   //  println!("{} {}" ,PI_NUMBER, age); // {]} ve age ile çağırdım veya PI Number ile iki kez çağırma
 //} 
+/* ders1 tekrar */
+//fn main ()  {
+ //    println!("Hello world!");}
+//ders 3 i8 signed u8 unsigned but 8 bit 816 16 bit 32 bit 64 bit 128 bit diye gidiyor u128 vb.
+// arch isize usize 255 0 arası 2n-1 signed sayılar -128 127 -(2n-1) 2n-1-1
+// ihtiyacınıza uygun değeri seçin büyük olursa 16x maliyet arttırmış
