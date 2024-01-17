7e70400778d1964399b3eeb53cc9dfe21d63337b
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-17 18:53:08 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
+// https://www.youtube.com/watch?v=0t_jpl70Mbg&list=PLgvWD2scL860_6ppZQS6i86vQuX_5wV2
+// Main fonksiyonunu oluşturmak istediğimiz için function main dekserasyonu oluşturuyoruz ve parantezler içerisinde de arıyorum ilk başta main fonksiyonunu arıyor birde süslü parantezleri arıyoruz
+//tam olarak fonksiyon denemez kırmızı çizgimiz noktalı ve her zaman koymalıyız fn main() {
     println!("Hello, world!");
 }
